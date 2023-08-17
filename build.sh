f793f20aa7e7e70345fd6523ff6369f2af839563
z-montlocs <agw.developer@gmail.com>
2023-08-17 15:58:11 -0500

    nine commit

--- build.sh
+++ build.sh
@@ -8,25 +8,53 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+CHROME_PATH=/opt/render/project/bin/chrome/opt/google/chrome/
 
-STORAGE_DIR=/opt/render/project/.render
+if [[ ! -d $CHROME_PATH ]]; then
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
+    echo "...Downloading Chrome Binary..."
+    #wget <uri> -P /path/to/folder
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
+
+    echo "...Installing Chrome Binary..."
+    mkdir -p /opt/render/project/bin/chrome
+    dpkg -x /tmp/google-chrome-stable_current_amd64.deb /opt/render/project/bin/chrome
+    # this is a directory with its own "etc", "opt", and "usr" subdir
+
+    echo "...Cleaning Up..."
+    rm /tmp/google-chrome-stable_current_amd64.deb
+
+    echo "...Adding to Path..."
+    #export PATH="${PATH}:${CHROME_PATH}/opt/google/chrome/"
+    export PATH="${PATH}:/opt/render/project/bin/chrome/opt/google/chrome"
+
 else
-  echo "...Using Chrome from cache"
+  echo "...Detected Existing Chrome Binary"
 fi
 
-# be sure to add Chromes location to the PATH as part of your Start Command
- export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
-# add your own build commands...
+#CHROMEDRIVER_PATH=/usr/local/bin/chromedriver
+CHROMEDRIVER_PATH=/opt/render/project/bin/chromedriver
+
+if [[ ! -d $CHROMEDRIVER_PATH ]]; then
+
+    echo "...Downloading Chromedriver..."
+    wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+
+    echo "...Installing Chromedriver..."
+    unzip /tmp/chromedriver.zip chromedriver -d /opt/render/project/bin
+
+    echo "...Cleaning Up..."
+    rm /tmp/chromedriver.zip
+
+    echo "...Adding to Path..."
+    export PATH="${PATH}:${CHROMEDRIVER_PATH}"
+    echo $PATH
+else
+  echo "...Detected Existing Chromedriver Installation"
+fi
+
+echo "...Installing packages..."
+pip install -r requirements.txt
+
+echo "...Build Script Completed!"
