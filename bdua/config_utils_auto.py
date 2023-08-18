a7ccda4fe660b04c4c4cbd991c387ba0740345e9
z-montlocs <agw.developer@gmail.com>
2023-08-18 07:12:35 -0500

    v1-2 commit

--- bdua/config_utils_auto.py
+++ bdua/config_utils_auto.py
@@ -57,9 +57,12 @@ def initialize_driver():
     try:
         # Opciones para el navegador Chrome
         options = webdriver.ChromeOptions()
-        options.add_argument("--disable-gpu")
-        options.add_argument("--headless")  # 
         options.add_argument("--no-sandbox")
+        options.add_argument("--headless")  
+        options.add_argument("--disable-extensions") 
+
+        options.add_argument("--disable-gpu")
+        
 
 
         # Inicializar el controlador de Chrome
