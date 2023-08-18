622b33f19f0ac26fe09f7046835acf5a5fa8e874
z-montlocs <agw.developer@gmail.com>
2023-08-17 23:24:34 -0500

    v-37 commit

--- build.sh
+++ build.sh
@@ -15,51 +15,58 @@ python manage.py migrate
 CHROME_PATH="/opt/render/project/bin/chrome/opt/google/chrome"
 CHROMEDRIVER_PATH="/opt/render/project/bin"
 
-if [[ ! -d $CHROME_PATH ]]; then
-    echo "...Downloading Chrome Binary..."
-    wget -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-    echo "...Installing Chrome Binary..."
-    mkdir -p /opt/render/project/bin/chrome
-    dpkg -x /tmp/google-chrome.deb /opt/render/project/bin/chrome
-
-    echo "...Cleaning Up..."
-    rm /tmp/google-chrome.deb
-
-    echo "...Adding Chrome to Path..."
-    export PATH="${PATH}:${CHROME_PATH}"
-    echo "Installed Chrome Version:"
-    google-chrome --version
-else
-    echo "...Detected Existing Chrome Binary"
+# Desinstalar Google Chrome si existe
+if [[ -d $CHROME_PATH ]]; then
+    echo "Uninstalling Existing Chrome Binary..."
+    rm -r $CHROME_PATH
+    echo "Existing Chrome Uninstalled."
 fi
 
-
-
-if [[ ! -d $CHROMEDRIVER_PATH ]]; then
-    echo "Downloading Chromedriver..."
-    CHROMEDRIVER_URL="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip"
-    wget -O /tmp/chromedriver.zip "$CHROMEDRIVER_URL"
-
-    echo "Installing Chromedriver..."
-    unzip /tmp/chromedriver.zip -d /opt/render/project/bin
-
-    echo "Cleaning Up..."
-    rm /tmp/chromedriver.zip
-
-    echo "Adding Chromedriver to Path..."
-    export PATH="${PATH}:${CHROMEDRIVER_PATH}"
-    echo "Installed Chromedriver Version:"
-    chromedriver --version
-else
-    echo "Detected Existing Chromedriver Installation"
+# Desinstalar Chromedriver si existe
+if [[ -d $CHROMEDRIVER_PATH ]]; then
+    echo "Uninstalling Existing Chromedriver..."
+    rm -r $CHROMEDRIVER_PATH
+    echo "Existing Chromedriver Uninstalled."
 fi
+
+# Instalar la nueva versión de Google Chrome
+echo "...Downloading Chrome Binary..."
+wget -O /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+echo "...Installing Chrome Binary..."
+mkdir -p /opt/render/project/bin/chrome
+dpkg -x /tmp/google-chrome.deb /opt/render/project/bin/chrome
+
+echo "...Cleaning Up..."
+rm /tmp/google-chrome.deb
+
+echo "...Adding Chrome to Path..."
+export PATH="${PATH}:${CHROME_PATH}"
+echo "Installed Chrome Version:"
+google-chrome --version
+
+# Instalar la nueva versión de Chromedriver
+echo "Downloading Chromedriver..."
+CHROMEDRIVER_URL="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip"
+wget -O /tmp/chromedriver.zip "$CHROMEDRIVER_URL"
+
+echo "Installing Chromedriver..."
+unzip /tmp/chromedriver.zip -d /opt/render/project/bin
+
+echo "Cleaning Up..."
+rm /tmp/chromedriver.zip
+
+echo "Adding Chromedriver to Path..."
+export PATH="${PATH}:${CHROMEDRIVER_PATH}"
+echo "Installed Chromedriver Version:"
+chromedriver --version
 
 echo "Installing packages..."
 # Puedes descomentar esta línea para instalar los paquetes de Python desde requirements.txt
 # pip install -r requirements.txt
 
 echo "Build Script Completed!"
+
 
 
 
