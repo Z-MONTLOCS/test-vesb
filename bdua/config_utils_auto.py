499f80eb9ca3d7f4d8cbb2b4d806fb1f7b59b8bf
z-montlocs <agw.developer@gmail.com>
2023-08-18 14:26:20 -0500

    v1-42 commit

--- bdua/config_utils_auto.py
+++ bdua/config_utils_auto.py
@@ -1,13 +1,12 @@
-# ✅ for selenium 4
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 
-driver = webdriver.Chrome(service=ChromiumService(
-    ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
-
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("http://www.python.org")
+
+time.sleep(2)
 
 driver.close()
