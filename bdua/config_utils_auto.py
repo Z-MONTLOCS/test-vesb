642979055ba138d11a807933597d47fe4a94a28a
z-montlocs <agw.developer@gmail.com>
2023-08-18 07:34:22 -0500

    v1-4 commit

--- bdua/config_utils_auto.py
+++ bdua/config_utils_auto.py
@@ -60,7 +60,7 @@ def initialize_driver():
         options.add_argument("--no-sandbox")
         options.add_argument("--headless")  
         options.add_argument("--disable-extensions") 
-        options.addArguments("--disable-dev-shm-usage");
+        options.addArgument("--disable-dev-shm-usage");
 
         options.add_argument("--disable-gpu")
         
