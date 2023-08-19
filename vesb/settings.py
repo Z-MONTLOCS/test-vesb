ee7cee5b45281583dfe3cc8937a15384fa607dce
z-montlocs <agw.developer@gmail.com>
2023-08-19 01:24:20 -0500

    v6.20 commit

--- vesb/settings.py
+++ vesb/settings.py
@@ -43,7 +43,8 @@ DEBUG = 'RENDER' not in os.environ
 
 CHROME_PATH = os.environ.get('CHROME_PATH',  default='/opt/render/project/bin/chrome-linux64')
 
-CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH',  default='/opt/render/project/bin/chromedriver-linux64')
+CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH',  default='/opt/render/project/bin/chromedriver')
+
 
 
 
