c1d98263383d029f04a2a711431617f712133d01
z-montlocs <agw.developer@gmail.com>
2023-08-18 20:40:33 -0500

    v6.12 commit

--- build.sh
+++ build.sh
@@ -15,21 +15,17 @@ echo "...Build Script Completed!"
 
 STORAGE_DIR=/opt/render/project/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
-else
-  echo "...Using Chrome from cache=>"
+echo "...Downloading Chrome"
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+rm ./google-chrome-stable_current_amd64.deb
+cd $HOME/project/src # Make sure we return to where we were
 
-  # Get path to Chrome executable
-  CHROME_EXECUTABLE_PATH=$(which google-chrome)
-  echo "Chrome executable path: $CHROME_EXECUTABLE_PATH"
-fi
+# Get path to Chrome executable
+CHROME_EXECUTABLE_PATH="/opt/render/project/.render/chrome/opt/google/chrome/google-chrome"
+echo "Chrome executable path: $CHROME_EXECUTABLE_PATH"
 
 # be sure to add Chrome's location to the PATH as part of your Start Command
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
