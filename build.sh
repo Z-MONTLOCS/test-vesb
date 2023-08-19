378e0410e6c569ada2209c97c11ce2b34a7c1426
z-montlocs <agw.developer@gmail.com>
2023-08-19 00:34:25 -0500

    v6.20 commit

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ if [[ ! -d $CHROME_PATH ]]; then
     export PATH="${PATH}:${CHROME_PATH}"
 
     echo "Installed Chrome Version:"
-    $CHROME_PATH/chrome --version
+    $CHROME_PATH --version
 else
     echo "...Detected Existing Chrome Binary"
 fi
