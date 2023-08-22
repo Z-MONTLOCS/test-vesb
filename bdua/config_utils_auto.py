9e098706205db20f45d48529e95eacd6afd7cde8
z-montlocs <agw.developer@gmail.com>
2023-08-22 15:12:01 -0500

    v8.1 commit

--- bdua/config_utils_auto.py
+++ bdua/config_utils_auto.py
@@ -34,17 +34,15 @@ def initialize_driver():
 
         print("************ INICIO VERSION **************")
 
-        # Rutas de los ejecutables
-        CHROME_PATH = os.path.join(os.environ.get('CHROME_PATH', '/opt/render/project/bin'), 'chrome-linux64')
-        CHROMEDRIVER_PATH = os.path.join(os.environ.get('CHROMEDRIVER_PATH', '/opt/render/project/bin'), 'chromedriver-linux64')
+        CHROME_PATH = os.environ.get('CHROME_PATH', '/opt/render/project/bin/chrome-linux64')
+        CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/opt/render/project/bin/chromedriver-linux64')
 
         print("Installed Chromedriver Version:")
-        chromedriver_version_cmd = f"{CHROMEDRIVER_PATH}/chromedriver --version"
+        chromedriver_version_cmd = f"{CHROMEDRIVER_PATH}/chromedriver-linux64/chromedriver --version"
         chromedriver_version_output = subprocess.getoutput(chromedriver_version_cmd)
         print(chromedriver_version_output)
-
         print("Installed Chrome Version:")
-        chrome_version_cmd = f"{CHROME_PATH}/chrome --version"
+        chrome_version_cmd = f"{CHROME_PATH}/chrome-linux64/chrome --version"
         chrome_version_output = subprocess.getoutput(chrome_version_cmd)
         print(chrome_version_output)
 
