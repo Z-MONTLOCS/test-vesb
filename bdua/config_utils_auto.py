0be111d2ece0599315101d6ae30d0b68bda078d1
z-montlocs <agw.developer@gmail.com>
2023-08-22 19:05:33 -0500

    v8.4 commit

--- bdua/config_utils_auto.py
+++ bdua/config_utils_auto.py
@@ -184,8 +184,18 @@ def initialize_driver():
        
 
         options.binary_location =PATH_CHROME_PATH   #chrome binary location specified here
-        options.add_argument("--start-maximized") #open Browser in maximized mode
         options.add_argument("--no-sandbox") #bypass OS security model
+
+
+        options.add_argument('--headless')
+
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--ignore-ssl-errors=true")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--remote-debugging-port=9222")
+        
         options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
@@ -193,10 +203,11 @@ def initialize_driver():
 
         service = Service(executable_path=PATH_CHROMEDRIVER_PATH)
 
-        driver = webdriver.Chrome(service=service, options=options)
+        #driver = webdriver.Chrome(service=service, options=options)
     
 
-                
+        driver=webdriver.Chrome()
+       
  
         
 
